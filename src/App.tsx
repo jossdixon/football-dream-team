d86439c11389d5d2ba87a5ae50b4e7fbf2dacea7
Joss Dixon <josscdixon@gmail.com>
2022-02-07 15:24:37 +0000

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [player, setPlayer] = useState([
+    {
+      name: "Lionel Messi",
+      url: "https://editorial.uefa.com/resources/026f-13d74cefe05a-09e682c0c3d1-1000/paris_saint-germain_v_manchester_city_group_a_-_uefa_champions_league.jpeg",
+      position: "Attacker",
+      age: 34,
+      team: "PSG",
+      nationality: "Argentina",
+      notes: ""
+    },
+    {
+      name: "Édouard Mendy",
+      url: "https://e0.365dm.com/21/12/768x432/skysports-edouard-mendy-chelsea_5604133.jpg?20211204160327",
+      position: "Goalkeeper",
+      age: 29,
+      team: "Chelsea",
+      nationality: "Senegal",
+      notes: ""
+    }
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Football Dream Team</h1>
     </div>
   );
 }
